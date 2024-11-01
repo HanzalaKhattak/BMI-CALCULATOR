315a2e8559dc65c799e597a3f643396739972176
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-11-01 11:52:06 +0500

    Added a switch statment to show which catagory the user falls in acc to BMI

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ function calculateBMI(){
     if(height === 0){
         alert("Input a Valid Height!");
     }
+    let catagory;
+    switch (true) {
+        case (bmi < 18.5):
+            catagory = "Underweight";
+            break;
+        case(bmi >= 18.5 && bmi < 25 ):
+            catagory = "Normal Weight";
+            break;
+        case (bmi >= 25 && bmi < 30):
+            catagory = "Overweight";
+            break;
+        case (bmi >= 30):
+            catagory = "Obesity";
+            break;
+        default:
+            catagory = "Invlaid BMI!";
+            break;
+    }
+    
     bmi = (bmi.toFixed(2));
     var result = document.getElementById('result')
-    result.innerHTML = bmi
+    result.innerHTML = (`Your BMI is ${bmi} which falls in the catagory ${catagory}.`)
 }
